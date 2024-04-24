8dc63e70f524bc6de9dd2c6980c195ec5eb7d2ef
Derin Owoade <owoadederin6@gmail.com>
2024-04-24 21:34:01 +0100

    adjusting text animations

--- components/AnimateText.tsx
+++ components/AnimateText.tsx
@@ -9,31 +9,32 @@ interface AnimateTextProps {
 
 export default function AnimateText(props: AnimateTextProps) {
   return (
-    <AnimatePresence key={props.activeImage}>
-      <motion.div className="relative">
-        <motion.div className=" h-[176px] overflow-hidden">
-          <motion.div
-            initial={{ y: "100%" }}
-            animate={{ y: "0%" }}
-            exit={{ y: "-120%", transition: { duration: 0.5 } }}
-            transition={{ duration: 1, ease: [0.25, 1, 0.3, 1] }}
-            className="h-[176px]"
-          >
-            {props.topText}
-          </motion.div>
-        </motion.div>
-        <motion.div className="h-[176px] overflow-hidden">
-          <motion.div
-            initial={{ y: "100%" }}
-            animate={{ y: "0%" }}
-            exit={{ y: "-120%", transition: { duration: 0.2 } }}
-            transition={{ duration: 0.5, ease: [0.25, 1, 0.3, 1] }}
-            className="h-[176px]"
-          >
-            {props.bottomText}
-          </motion.div>
+    <motion.div className="relative">
+      <motion.div className=" h-[176px] overflow-hidden">
+        <motion.div
+          key={props.activeImage + "top"}
+          initial={{ y: "-120%" }}
+          animate={{ y: "0%" }}
+          exit={{ y: "120%", transition: { duration: 0.8 } }}
+          transition={{ duration: 1, ease: [0.25, 1, 0.3, 1] }}
+          className="h-[176px]"
+        >
+          {props.topText}
         </motion.div>
       </motion.div>
-    </AnimatePresence>
+
+      <motion.div className="h-[176px] overflow-hidden">
+        <motion.div
+          key={props.activeImage + "bottom text"}
+          initial={{ y: "-120%" }}
+          animate={{ y: "0%" }}
+          exit={{ y: "120%", transition: { duration: 0.5 } }}
+          transition={{ duration: 0.7, ease: [0.25, 1, 0.3, 1] }}
+          className="h-[176px]"
+        >
+          {props.bottomText}
+        </motion.div>
+      </motion.div>
+    </motion.div>
   );
 }
